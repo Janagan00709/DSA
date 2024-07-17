c51ae91c3045df2455f9dde1cedf0a1e9345e76a
Janagan00709 <janagan21.cse@sonatech.ac.in>
2024-07-17 19:17:54 +0530

    second commit

---
+++ Trees/BTLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+package Trees;
+//Leetcode->102
+import java.util.*;
+public class BTLevelOrderTraversal {
+    public static void main(String[] args) {
+        
+    }
+    public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+             TreeNode(int val) { this.val = val; }
+             TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevelList = new ArrayList<>(levelSize);
+            for(int i=0;i<levelSize;i++){
+                TreeNode currentNode = queue.poll();
+                currentLevelList.add(currentNode.val);
+                if(currentNode.left!=null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevelList);
+        }
+        return result;
+    }
+}
